357d13f2072210c794954a860b0bd850ca35f2ad
Vishal Suri <vishalsuri001@gmail.com>
2022-06-24 03:16:31 +0530

    Admin Panel Functionality template start

---
+++ resources/views/admin/admin_master.blade.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8" />
+        <title>Dashboard | Upcube - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('backend/') }}/assets/images/favicon.ico">
+
+        <!-- jquery.vectormap css -->
+        <link href="{{ asset('backend/') }}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+
+        <!-- DataTables -->
+        <link href="{{ asset('backend/') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Responsive datatable examples -->
+        <link href="{{ asset('backend/') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('backend/') }}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('backend/') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('backend/') }}/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body data-topbar="dark">
+
+    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+            @include('admin.partials.header')
+
+            <!-- ========== Left Sidebar Start ========== -->
+            <div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+
+                    <!-- User details -->
+                    {{-- <div class="user-profile text-center mt-3">
+                        <div class="">
+                            <img src="{{ asset('backend/') }}/assets/images/users/avatar-1.jpg" alt="" class="avatar-md rounded-circle">
+                        </div>
+                        <div class="mt-3">
+                            <h4 class="font-size-16 mb-1">Julia Hudda</h4>
+                            <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+                        </div>
+                    </div> --}}
+
+                    <!--- Sidemenu -->
+                    @include('admin.partials.sidebar-left')
+                    <!-- Sidebar -->
+                </div>
+            </div>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                @yield('admin')
+                <!-- End Page-content -->
+                @include('admin.partials.footer')
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        @include('admin.partials.sidebar-right')
+        <!-- /Right-bar -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('backend/') }}/assets/libs/jquery/jquery.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/node-waves/waves.min.js"></script>
+
+
+        <!-- apexcharts -->
+        <script src="{{ asset('backend/') }}/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <!-- jquery.vectormap map -->
+        <script src="{{ asset('backend/') }}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
+
+        <!-- Required datatable js -->
+        <script src="{{ asset('backend/') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+
+        <!-- Responsive examples -->
+        <script src="{{ asset('backend/') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="{{ asset('backend/') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+        <script src="{{ asset('backend/') }}/assets/js/pages/dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="{{ asset('backend/') }}/assets/js/app.js"></script>
+    </body>
+
+</html>
+
